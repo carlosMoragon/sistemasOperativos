b4b1dde5093ca15bff4882ea2dc95165135d806a
carlosMoragon <carlos.moragoncorella@gmail.com>
2023-03-01 06:23:18 -0800

    terminado tema 3

--- tema3IntroduccionUnix/ejemplo_ignorar_señal.c
+++ tema3IntroduccionUnix/ejemplo_ignorar_señal.c
@@ -6,21 +6,29 @@
 
 #define VUELTAS 100000000000
 
+/*
+
+SIG_DFL -> para restaurar su opcion por defecto
+SIG_IGN -> para ignorar la señal
+
+*/
+
 void Salir(int sig){
 
 	switch(sig){
 		case(SIGTERM):
-			printf("Saliendo del programa al recibir la señal\n");
+			printf("Saliendo del programa al recibir la señal SIGTERM\n");
 			//exit(EXIT_SUCCESS);
 			exit(0);
-			break;
 	}
 }
 
 void main (void){
 	
 	signal(SIGTERM, Salir); //Si recibimos la señal sigterm se ejecuta la función salir
-
+	signal(SIGTERM, SIG_IGN); //PARA IGNORAR LA SEÑAL ->siempre se imprime -> Saliendo del programa sin recibir la señal
+	
+	
 	for(int i= 0; i<VUELTAS; i++){} //Esto es para q tengamos tiempo de meter la señal
 	printf("Saliendo del programa sin recibir la señal\n");
 	//exit(EXIT_SUCCESS);
--- tema3IntroduccionUnix/ejemplo_señal_2.c
+++ tema3IntroduccionUnix/ejemplo_señal_2.c
@@ -14,13 +14,18 @@ void Salir(int sig){
 			//exit(EXIT_SUCCESS);
 			exit(0);
 			break;
+		case(SIGINT):
+			printf("Saliendo del programa al recibir la señal SIGINT\n");
+			//exit(EXIT_SUCCESS);
+			exit(0);
+			break;	
 	}
 }
 
 void main (void){
 	
 	signal(SIGTERM, Salir); //Si recibimos la señal sigterm se ejecuta la función salir
-
+	signal(SIGINT, Salir);
 	for(int i= 0; i<VUELTAS; i++){} //Esto es para q tengamos tiempo de meter la señal
 	printf("Saliendo del programa sin recibir la señal\n");
 	//exit(EXIT_SUCCESS);

1e303b03af6610d7fe27b082dce058e6d2c0de9d
carlosMoragon <carlos.moragoncorella@gmail.com>
2023-03-29 07:15:19 -0700

    consumidor productor

---
+++ tema5Semaforos/productor_consumidor.c
@@ -0,0 +1,59 @@
+#include <stddef.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <semaphore.h>
+
+sem_t productor;
+sem_t consumidor;
+sem_t mutex;
+
+int n_elementos = 0;
+
+void * consumidores(void * args){
+	int consumidos = 0;
+	while(consumidos != 20){
+		sem_wait(&consumidor);
+		sem_wait(&mutex);
+		n_elementos--;
+		consumidos++;
+		sem_post(&mutex);
+		printf("soy el consumidor\n");
+		fflush(stdout);
+		sem_post(&productor);
+		sleep(1);
+		
+	}
+}
+
+void * productores(void * args){
+	sem_wait(&productor);
+	sem_wait(&mutex);
+	n_elementos++;
+	sem_post(&mutex);
+	printf("soy el productor %d\n", *(int*)args);
+	fflush(stdout);
+	sem_post(&consumidor);
+	
+}
+
+
+void main(){
+	sem_init(&productor, 0, 5);
+	sem_init(&consumidor, 0, 0);
+	sem_init(&mutex, 0, 1);
+	
+	pthread_t consumidor;
+	pthread_t productor[20];
+	
+	pthread_create(&consumidor, NULL, consumidores, NULL);	
+	
+	for(int i = 0; i<20; i++){
+		pthread_create(&productor[i], NULL, productores, &i);
+		pthread_join(productor[i], NULL);
+		
+	}
+	
+}
--- tema5Semaforos/semaforo_ordenarHilos.c
+++ tema5Semaforos/semaforo_ordenarHilos.c
@@ -8,18 +8,24 @@
 
 sem_t mutex;
 
-void * proceso(void * args){
-	sem_wait(&mutex);
+void * proceso1(void * args){
 	printf("%s ", (char *)args);
 	sem_post(&mutex);
+	pthread_exit(0);
+}
+
+void * proceso2(void * args){
+	sem_wait(&mutex);
+	printf("%s ", (char *)args);
+	pthread_exit(0);
 }
 
 void main(){
-	sem_init(&mutex, 0, 1);
+	sem_init(&mutex, 0, 0);
 	pthread_t hilos[2];
 	
-	pthread_create(&hilos[0], NULL, proceso, "Hello");
-	pthread_create(&hilos[1], NULL, proceso, "World");
+	pthread_create(&hilos[0], NULL, proceso1, "Hello");
+	pthread_create(&hilos[1], NULL, proceso2, "World");
 //	sleep(1);
 	pthread_join(hilos[0], NULL);
 	pthread_join(hilos[1], NULL);

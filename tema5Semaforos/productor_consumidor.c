60e378edc98523d75183d0618435d84073879863
carlosMoragon <carlos.moragoncorella@gmail.com>
2023-04-17 08:05:59 -0700

    corrigiendo ejercicios

--- tema5Semaforos/productor_consumidor.c
+++ tema5Semaforos/productor_consumidor.c
@@ -6,6 +6,20 @@
 #include <fcntl.h>
 #include <semaphore.h>
 
+
+/*
+Para ello, suponga que tiene cinco huecos sobre los que puede producir e inicialmente no tiene
+datos para consumir. Los huecos y datos se implementarán a través de semáforos.
+Además, tendrá dos tipos de hilos: productores y consumidores.
+Los productores están asociados a la función producir (ejecutada cada segundo) y los
+consumidores a la función consumir (ejecutada cada cinco segundos), que gestionarán los
+huecos y datos de forma adecuada para cada caso, a través de las operaciones wait y signal. En
+total, habrá veinte productores y un consumidor.
+Tenga en cuenta que debe mostrar por pantalla el número del productor o consumidor y los
+elementos que contiene el buffer.
+*/
+
+
 sem_t productor;
 sem_t consumidor;
 sem_t mutex;
@@ -19,9 +33,11 @@ void * consumidores(void * args){
 		sem_wait(&mutex);
 		n_elementos--;
 		consumidos++;
-		sem_post(&mutex);
-		printf("soy el consumidor\n");
+		printf("soy el consumidor y el buffer tiene %d elementos\n", n_elementos);
 		fflush(stdout);
+		sem_post(&mutex);
+		//printf("soy el consumidor y el buffer tiene %d elementos\n", n_elementos);
+		//fflush(stdout);
 		sem_post(&productor);
 		sleep(1);
 		
@@ -32,9 +48,11 @@ void * productores(void * args){
 	sem_wait(&productor);
 	sem_wait(&mutex);
 	n_elementos++;
-	sem_post(&mutex);
-	printf("soy el productor %d\n", *(int*)args);
+	printf("soy el productor %d y el buffer tiene %d elementos\n", *(int*)args, n_elementos);
 	fflush(stdout);
+	sem_post(&mutex);
+	//printf("soy el productor %d y el buffer tiene %d elementos\n", *(int*)args, n_elementos);
+	//fflush(stdout);
 	sem_post(&consumidor);
 	
 }
@@ -50,12 +68,17 @@ void main(){
 	
 	pthread_create(&consumidor, NULL, consumidores, NULL);	
 	
+	int pos_array[20];
 	for(int i = 0; i<20; i++){
-		pthread_create(&productor[i], NULL, productores, &i);
-		pthread_join(productor[i], NULL);
-		
+		pos_array[i] = i;
 	}
 	
+	for(int i = 0; i<20; i++){
+		pthread_create(&productor[i], NULL, productores, &pos_array[i]);
+	}
+	pthread_join(consumidor, NULL);
+	
 }
+
 
 
